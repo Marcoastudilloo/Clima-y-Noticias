611d16ae42195aef09b1c85ef7f3750020946949
Marcoastudilloo <marcoantonioastudillo125@gmail.com>
2024-10-12 10:10:01 -0600

    app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
 @app.route('/api/clima')
 def get_clima():
     ciudad = request.args.get('ciudad', 'Acapulco')  # Valor por defecto
-    api_key = 'a64b8a2053b9ea3ae8a8460978ac141c'  # Reemplaza con tu clave de API
+    api_key = ''  # Reemplaza con tu clave de API
     response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={api_key}&units=metric')
 
     if response.status_code == 200:  # Manejo de errores
@@ -21,7 +21,7 @@ def get_clima():
 @app.route('/api/noticias')
 def get_noticias():
     ciudad = request.args.get('ciudad', 'Acapulco')  # Asegúrate de que esté aquí
-    api_key = 'a7556ff674714937b74b5b10b705937b'  # Reemplaza con tu clave de API
+    api_key = ''  # Reemplaza con tu clave de API
     response = requests.get(f'https://newsapi.org/v2/everything?q={ciudad}&from=2024-10-10&sortBy=popularity&apiKey={api_key}')
 
     if response.status_code == 200:  # Manejo de errores
